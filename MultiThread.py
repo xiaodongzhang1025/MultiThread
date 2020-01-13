20be4f3d14af5398c42716f989378d62e049ce0a
xiaodongzhang <xiaodongzhang1025@163.com>
2020-01-13 14:03:03 +0800

    add support for multiprocess.

--- MultiThread.py
+++ MultiThread.py
@@ -13,21 +13,9 @@ import multiprocessing
 reload(sys)
 sys.setdefaultencoding('utf8')
     
-def GetDataEncoding(data):
-    data_encoding = None
-    det_ret = chardet.detect(data)
-    if det_ret:
-        data_encoding = det_ret['encoding']
-        #print det_ret
-        #print 'data_encoding', data_encoding
-    if data_encoding == 'windows-1251':
-        data_encoding = 'gbk'
-        #print '    windows-1251 ==> gbk'
-    return data_encoding
-    
 def TestThreadProc(para):
     try:
-        print threading.current_thread().name, para, 'Enter!!!'
+        print threading.current_thread().name, 'Enter!!!'
         time.sleep(2)
     except Exception, err:
         #print err
@@ -36,7 +24,19 @@ def TestThreadProc(para):
     finally:
         #pass
         print threading.current_thread().name, 'Exit!!!'
-    
+
+def TestProcessProc(para):
+    try:
+        print multiprocessing.current_process().name, 'Enter!!!'
+        time.sleep(2)
+    except Exception, err:
+        #print err
+        print '===> Exception'
+        print str(err).decode("string_escape")
+    finally:
+        #pass
+        print multiprocessing.current_process().name, 'Exit!!!'
+        
 def MultiThreadStart(para_all, ThreadProc, thread_num_max = None):
     thread_list = []
     try:
@@ -68,7 +68,37 @@ def MultiThreadStart(para_all, ThreadProc, thread_num_max = None):
         print str(err).decode("string_escape")
     finally:
         return thread_list
-    
+        
+def MultiProcessStart(para_all, ProcessProc, process_num_max = None):
+    process_list = []
+    try:
+        if process_num_max == None or process_num_max < 1:
+            process_num_max = multiprocessing.cpu_count()
+        process_num = process_num_max
+        if process_num > len(para_all):
+            process_num = len(para_all)
+            
+        for i in range(0, process_num):
+            para_all_len = len(para_all)
+            para_len_average = int((para_all_len + process_num - 1)/process_num)
+            para_len_cure = para_len_average
+            if i == process_num - 1:
+                para_len_cure = para_all_len - (i*para_len_average)
+            
+            para = para_all[(i*para_len_average):(i*para_len_average + para_len_cure)]
+            print '\n----------------------------------'
+            print '===>', i, para_len_cure#, para
+            process = multiprocessing.Process(target=ProcessProc, name='process_%d'%i, kwargs = {'para':para})
+            process_list.append(process)
+            process.start()
+        
+    except Exception, err:
+        #print err
+        print '===> Exception'
+        print str(err).decode("string_escape")
+    finally:
+        return process_list
+
 if "__main__" == __name__:
     print '\n------------------------------The Start-----------------------------'
     start_time = time.clock()
@@ -77,20 +107,23 @@ if "__main__" == __name__:
     #####################################################
     try:
         thread_num_max = None
+        process_num_max = None
+        
         para_all = []
-        for i in range(0, 900):
+        for i in range(0, 90):
             para_all.append(i)
         
-        #thread_list = MultiThreadStart(para_all, TestThreadProc)
-        thread_list = MultiThreadStart(para_all, TestThreadProc, thread_num_max)
-        for thread in thread_list:
-            thread.join()
+        #thread_list = MultiThreadStart(para_all, TestThreadProc, thread_num_max)
+        task_list = MultiProcessStart(para_all, TestProcessProc, process_num_max)
+        
+        #print 'active thread count', len(threading.enumerate())#threading.active_count()
+        print 'active process count', len(multiprocessing.active_children())
+        
+        for task in task_list:
+            task.join()
             
-        # while True:
-            # active_count = threading.active_count()
-            # active_count = len(threading.enumerate())
-            # if active_count == 1:
-                # break
+        #print 'active thread count', len(threading.enumerate()) #threading.active_count()
+        print 'active process count', len(multiprocessing.active_children())
     except Exception, err:
         #print err
         print '===> Exception'
